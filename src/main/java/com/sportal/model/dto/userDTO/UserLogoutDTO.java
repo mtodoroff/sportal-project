e265a974877475add1bda89f271299f22b82e7e1
mtodoroff <32134525+mtodoroff@users.noreply.github.com>
2022-02-16 23:46:33 +0200

    Added UserLogoutDTO with appropriate message

---
+++ src/main/java/com/sportal/model/dto/userDTO/UserLogoutDTO.java
@@ -0,0 +1,17 @@
+package com.sportal.model.dto.userDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+public class UserLogoutDTO {
+    private String msg;
+
+}
