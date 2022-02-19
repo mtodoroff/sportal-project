5245d4c2a61d23aed9b7cd3256231c573dc71083
mtodoroff <32134525+mtodoroff@users.noreply.github.com>
2022-02-19 16:13:26 +0200

    Added CustomHandlerExceprion, removed ErrorDTO from /userDTO

--- src/main/java/com/sportal/controller/CustomHandlerException.java
+++ src/main/java/com/sportal/controller/CustomHandlerException.java
@@ -1,24 +1,73 @@
 package com.sportal.controller;
 
+import com.sportal.exceptions.BadRequestException;
+import com.sportal.exceptions.NotFoundException;
 import com.sportal.exceptions.UnauthorizedException;
-import com.sportal.model.dto.userDTO.ErrorDTO;
+import com.sportal.model.dto.ErrorDTO;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class CustomHandlerException extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value = {UnauthorizedException.class})
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    @ResponseBody
-    public ErrorDTO errorUnauthorized(Exception e) {
-        ErrorDTO dto = new ErrorDTO();
-        dto.setMsg(e.getMessage());
-        dto.setStatusCode(HttpStatus.UNAUTHORIZED.value());
-        return dto;
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<ErrorDTO> handleUnauthorized(Exception e){
+        ErrorDTO errorDTO = new ErrorDTO();
+        errorDTO.setStatus(HttpStatus.UNAUTHORIZED);
+        errorDTO.setMsg(e.getMessage());
+        errorDTO.setDateTime(LocalDateTime.now());
+        return new ResponseEntity<>(errorDTO, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorDTO> handleBadRequest(Exception e){
+        ErrorDTO errorDTO = new ErrorDTO();
+        errorDTO.setStatus(HttpStatus.BAD_REQUEST);
+        errorDTO.setMsg(e.getMessage());
+        errorDTO.setDateTime(LocalDateTime.now());
+        return new ResponseEntity<>(errorDTO, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDTO> handleMethodArgumentNotValid(MethodArgumentNotValidException e){
+        ErrorDTO errorDTO = new ErrorDTO();
+        errorDTO.setStatus(HttpStatus.BAD_REQUEST);
+
+        List<String> errorMessages = e.getFieldErrors()
+                .stream().map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        errorDTO.setMsg(String.join("; ", errorMessages));
+        errorDTO.setDateTime(LocalDateTime.now());
+        return new ResponseEntity<>(errorDTO, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorDTO> handleNotFound(Exception e){
+        ErrorDTO errorDTO = new ErrorDTO();
+        errorDTO.setStatus(HttpStatus.NOT_FOUND);
+        errorDTO.setMsg(e.getMessage());
+        errorDTO.setDateTime(LocalDateTime.now());
+        return new ResponseEntity<>(errorDTO, HttpStatus.NOT_FOUND);
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDTO> handleOtherExceptions(Exception e){
+        ErrorDTO errorDTO = new ErrorDTO();
+        errorDTO.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+        errorDTO.setMsg("Unexpected error: " + e.getMessage());
+        e.printStackTrace();
+        errorDTO.setDateTime(LocalDateTime.now());
+        return new ResponseEntity<>(errorDTO, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- src/main/java/com/sportal/model/dto/ErrorDTO.java
+++ src/main/java/com/sportal/model/dto/ErrorDTO.java
@@ -1,13 +1,19 @@
-package com.sportal.model.dto.userDTO;
+package com.sportal.model.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class ErrorDTO extends RuntimeException{
+    private HttpStatus status;
     private String msg;
-    private int statusCode;
+    private LocalDateTime dateTime;
+
+
 }
