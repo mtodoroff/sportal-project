135578647e4217bf59370782a47827821c868c24
mtodoroff <32134525+mtodoroff@users.noreply.github.com>
2022-02-21 14:14:25 +0200

    Added Validation to CommentDTOS

--- src/main/java/com/sportal/model/dto/commentDTOs/CommentAddRequestDTO.java
+++ src/main/java/com/sportal/model/dto/commentDTOs/CommentAddRequestDTO.java
@@ -5,11 +5,14 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
 
+import javax.validation.constraints.NotBlank;
+
 @NoArgsConstructor
 @Getter
 @Setter
 @Component
 public class CommentAddRequestDTO {
     private long articleId;
+    @NotBlank(message = "Comment field is cannot be empty")
     private String comment_text;
 }
--- src/main/java/com/sportal/model/dto/commentDTOs/CommentEditRequestDTO.java
+++ src/main/java/com/sportal/model/dto/commentDTOs/CommentEditRequestDTO.java
@@ -5,11 +5,14 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @Component
 public class CommentEditRequestDTO {
     private long id;
+    @NotBlank(message = "Comment field is cannot be empty")
     private String comment_text;
 }
