2bbdb12ea3908ff8da10dee5eca72794eed8e3b8
mitko851111 <mitko851111@abv.bg>
2022-02-21 16:54:51 +0200

    Add method in Artical Repository

--- src/main/java/com/sportal/model/repository/ArticleRepository.java
+++ src/main/java/com/sportal/model/repository/ArticleRepository.java
@@ -2,14 +2,21 @@ package com.sportal.model.repository;
 
 import com.sportal.model.pojo.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<Article,Long> {
 
  Article findByContent(String content);
- Article findByTitle(String title);
+
+
+ @Query(value = "FROM Article  WHERE title LIKE %:title%")
+ List<Article> findByTitleUsingLike(@Param("title") String title) ;
+
  @Query(value = "SELECT * FROM articles ORDER BY views DESC LIMIT 5",nativeQuery = true)
  List<Article> findTopByViews();
  @Query(value = "SELECT * FROM articles ORDER BY created_at DESC LIMIT 5;",nativeQuery = true)
