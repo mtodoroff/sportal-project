e9f9bbf87268c14d08c5f131d0e7f1defb1da3e3
mitko851111 <mitko851111@abv.bg>
2022-02-22 14:09:57 +0200

    Refactor

--- src/main/java/com/sportal/model/repository/ArticleRepository.java
+++ src/main/java/com/sportal/model/repository/ArticleRepository.java
@@ -14,17 +14,10 @@ public interface ArticleRepository extends JpaRepository<Article,Long> {
 
  Article findByContent(String content);
 
-
  @Query(value = "FROM Article  WHERE title LIKE %:title%")
  List<Article> findByTitleUsingLike(@Param("title") String title) ;
  @Query(value = "SELECT * FROM articles ORDER BY views DESC LIMIT 5",nativeQuery = true)
  List<Article> findTopByViews();
  @Query(value = "SELECT * FROM articles ORDER BY created_at DESC LIMIT 5;",nativeQuery = true)
  List<Article> latestFiveArticles();
- /*
- @Modifying
- @Query("delete from Article a where a.id=:articleId")
- void deleteArticle(@Param("articleId") Long articleId);
-
-  */
 }
